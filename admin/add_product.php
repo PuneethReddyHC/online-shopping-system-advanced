77a33bb3488ccdf2e02c9e84091b3bd0e3ee61d8
Akintunde-Rotimi <tundeolawale71@yahoo.com>
2020-04-09 11:05:20 +0200

    Fixed SQL Injection Vulnerabilities
    removed extra double quote on style attribute on the add product button. fixed sql vulnerabilities. fixed formatting. added check for file if not empty.

--- admin/add_product.php
+++ admin/add_product.php
@@ -1,106 +1,114 @@
 <?php
-
 include("../db.php");
 session_start();
 
 
-if(isset($_POST['submit']))
-{
-$product_name=$_POST['product_name'];
-$details=$_POST['details'];
-$price=$_POST['price'];
-$c_price=$_POST['c_price'];
-$product_type=$_POST['product_type'];
-$brand=$_POST['brand'];
-$tags=$_POST['tags'];
+if (isset($_POST['submit'])) {
+    $product_name = $_POST['product_name'];
+    $details = $_POST['details'];
+    $price = $_POST['price'];
+    $c_price = $_POST['c_price'];
+    $product_type = $_POST['product_type'];
+    $brand = $_POST['brand'];
+    $tags = $_POST['tags'];
+    $pic_name = ''; //default pic name
+    //check if file was uploaded
+    if (!empty($_FILES['picture'])) {
+        //picture coding
+        $picture_name = $_FILES['picture']['name'];
+        $picture_type = $_FILES['picture']['type'];
+        $picture_tmp_name = $_FILES['picture']['tmp_name'];
+        $picture_size = $_FILES['picture']['size'];
 
-//picture coding
-$picture_name=$_FILES['picture']['name'];
-$picture_type=$_FILES['picture']['type'];
-$picture_tmp_name=$_FILES['picture']['tmp_name'];
-$picture_size=$_FILES['picture']['size'];
+        if ($picture_type == "image/jpeg" || $picture_type == "image/jpg" || $picture_type == "image/png" || $picture_type == "image/gif") {
+            if ($picture_size <= 50000000) {
+                $pic_name = time() . "_" . $picture_name;
+            }
+            move_uploaded_file($picture_tmp_name, "../product_images/" . $pic_name);
+        }
+    }
 
-if($picture_type=="image/jpeg" || $picture_type=="image/jpg" || $picture_type=="image/png" || $picture_type=="image/gif")
-{
-	if($picture_size<=50000000)
-	
-		$pic_name=time()."_".$picture_name;
-		move_uploaded_file($picture_tmp_name,"../product_images/".$pic_name);
-		
-mysqli_query($con,"insert into products (product_cat, product_brand,product_title,product_price, product_desc, product_image,product_keywords) values ('$product_type','$brand','$product_name','$price','$details','$pic_name','$tags')") or die ("query incorrect");
+    //save to DB using prepared statements
+    // prepare and bind
+    $stmt = $con->prepare("insert into products (product_cat, product_brand,product_title,product_price, product_desc, product_image,product_keywords) values (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssss", $product_type, $brand, $product_name, $price, $details, $pic_name, $tags);
+    $insert_qry_result = $stmt->execute();
+    if ($insert_qry_result === false) {
+        error_log('The insert query failed. ' . PHP_EOL . mysqli_error($con));
+        die('error occured');
+    }
 
- header("location: sumit_form.php?success=1");
+    header("location: sumit_form.php?success=1");
 }
 
 mysqli_close($con);
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Admin Panel</title>
- <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="style/css/bootstrap.min.css" rel="stylesheet">
-<link href="style/css/k.css" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Admin Panel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="style/css/bootstrap.min.css" rel="stylesheet">
+        <link href="style/css/k.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 
-</head>
-<body>
- 
-   	 <?php include("include/header.php");?>
-   	<div class="container-fluid">
-	<?php include("include/side_bar.php");?>
-    <div class="col-md-9 content" style="margin-left:10px">
-  	<div class="panel panel-default">
-	<div class="panel-heading" style="background-color:#c4e17f">
-	<h1><span class="glyphicon glyphicon-tag"></span> Product / Add Product  </h1></div><br>
-	<div class="panel-body" style="background-color:#E6EEEE;">
-		<div class="col-lg-7">
-        <div class="well">
-        <form action="add_product.php" method="post" name="form" enctype="multipart/form-data">
-        <p>Title</p>
-        <input class="input-lg thumbnail form-control" type="text" name="product_name" id="product_name" autofocus style="width:100%" placeholder="Product Name" required>
-<p>Description</p>
-<textarea class="thumbnail form-control" name="details" id="details" style="width:100%; height:100px" placeholder="write here..." required></textarea>
-<p>Add Image</p>
-<div style="background-color:#CCC">
-<input type="file" style="width:100%" name="picture" class="btn thumbnail" id="picture" >
-</div>
-</div>
-<div class="well">
-<h3>Pricing</h3>
-<p>Price</p>
-<div class="input-group">
-      <div class="input-group-addon">Rs</div>
-      <input type="text" class="form-control" name="price" id="price"  placeholder="0.00" required>
-    </div><br>
+    </head>
+    <body>
 
+<?php include("include/header.php"); ?>
+        <div class="container-fluid">
+        <?php include("include/side_bar.php"); ?>
+            <div class="col-md-9 content" style="margin-left:10px">
+                <div class="panel panel-default">
+                    <div class="panel-heading" style="background-color:#c4e17f">
+                        <h1><span class="glyphicon glyphicon-tag"></span> Product / Add Product  </h1></div><br>
+                    <div class="panel-body" style="background-color:#E6EEEE;">
+                        <div class="col-lg-7">
+                            <div class="well">
+                                <form action="add_product.php" method="post" name="form" enctype="multipart/form-data">
+                                    <p>Title</p>
+                                    <input class="input-lg thumbnail form-control" type="text" name="product_name" id="product_name" autofocus style="width:100%" placeholder="Product Name" required>
+                                    <p>Description</p>
+                                    <textarea class="thumbnail form-control" name="details" id="details" style="width:100%; height:100px" placeholder="write here..." required></textarea>
+                                    <p>Add Image</p>
+                                    <div style="background-color:#CCC">
+                                        <input type="file" style="width:100%" name="picture" class="btn thumbnail" id="picture" >
+                                    </div>
+                            </div>
+                            <div class="well">
+                                <h3>Pricing</h3>
+                                <p>Price</p>
+                                <div class="input-group">
+                                    <div class="input-group-addon">Rs</div>
+                                    <input type="text" class="form-control" name="price" id="price"  placeholder="0.00" required>
+                                </div><br>
+                            </div>
+                        </div>  
+                        <div class="col-lg-5">
+                            <div class="well">
+                                <h3>Category</h3>  
+                                <p>Product type</p>
+                                <input type="number" name="product_type" id="product_type" class="form-control" placeholder="1 electronic,2 Ladies Wears,3 Mens Wear">
+                                <br>
+                                <p>Vendor / Brand</p>
+                                <input type="number" name="brand" id="brand" class="form-control" placeholder="1 HP,2 Samsung,3 Apple,4 motorolla">
+                                <br>
+                                <p>Other tags</p>
+                                <input type="text" name="tags" id="tags" class="form-control" placeholder="Summer, Soft, Cotton etc">
+                            </div>          
+                        </div>
 
-    </div>
-        </div>  
-        <div class="col-lg-5">
-        <div class="well">
-<h3>Category</h3>  
-<p>Product type</p>
-<input type="number" name="product_type" id="product_type" class="form-control" placeholder="1 electronic,2 Ladies Wears,3 Mens Wear">
-<br>
-<p>Vendor / Brand</p>
-<input type="number" name="brand" id="brand" class="form-control" placeholder="1 HP,2 Samsung,3 Apple,4 motorolla">
-<br>
-<p>Other tags</p>
-<input type="text" name="tags" id="tags" class="form-control" placeholder="Summer, Soft, Cotton etc">
-</div>          
-</div>
+                        <div align="center">
+                            <button type="submit" name="submit" id="submit" class="btn btn-default" style="width:100px; height:60px"> Cancel</button>
+                            <button type="submit" name="submit" id="submit" class="btn btn-success" style="width:150px; height:60px""> Add Product</button>
+                        </div>
+                        </form>
 
-<div align="center">
-    <button type="submit" name="submit" id="submit" class="btn btn-default" style="width:100px; height:60px"> Cancel</button>
-    <button type="submit" name="submit" id="submit" class="btn btn-success" style="width:150px; height:60px""> Add Product</button>
-    </div>
-        </form>
- 
-	</div>
-</div></div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
 <?php include("include/js.php"); ?>
-</body>
+    </body>
 </html>
